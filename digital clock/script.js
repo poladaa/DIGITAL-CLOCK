fa7f6094f1de86b82cb2d00805f880709286a5a2
luka <120731202+poladaa@users.noreply.github.com>
2023-05-12 13:56:22 +0400

    Add files via upload

---
+++ digital clock/script.js
@@ -0,0 +1,32 @@
+"use strict";
+const hour = document.getElementById("hour");
+const minutes = document.getElementById("minutes");
+const seconds = document.getElementById("seconds");
+const ampmEl = document.getElementById("ampm");
+////
+
+function updateClock() {
+  let h = new Date().getHours();
+  let m = new Date().getMinutes();
+  let s = new Date().getSeconds();
+  let ampm = "AM";
+
+  if (h > 12) {
+    h = h - 12;
+    ampm = "PM";
+  }
+
+  h = h < 10 ? "0" + h : h;
+  m = m < 10 ? "0" + m : m;
+  s = s < 10 ? "0" + s : s;
+
+  hour.textContent = h;
+  minutes.textContent = m;
+  seconds.textContent = s;
+  ampmEl.textContent = ampm;
+  setTimeout(() => {
+    updateClock();
+  }, 1000);
+}
+
+updateClock();
